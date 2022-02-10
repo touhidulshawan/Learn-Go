27fce4fde456eb1354b47ef2fb0ee9af3f2d7d7f
Touhidul Shawan <touhidulshawan@gmail.com>
2022-02-10 18:12:42 +0600

    building booking app

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -6,9 +6,12 @@ func main() {
 	fmt.Println("Welcome to our Booking App")
 
 	conferenceName := "Love Yourself"
+	var availableTicket uint32
+	availableTicket = 200
 
 	fmt.Printf("Buy tickets for current conference: %v\n", conferenceName)
-    fmt.Println("----------------------------------------")
+	fmt.Printf("Available Tickets : %v\n", availableTicket)
+	fmt.Println("----------------------------------------")
 
 	// take information from user
 
@@ -25,13 +28,16 @@ func main() {
 	fmt.Scan(&userEmail)
 
 	fmt.Print("How many tickets you want to buy: ")
-	var ticketToBuy int32
+	var ticketToBuy uint32
 	fmt.Scan(&ticketToBuy)
 
-    fmt.Println()
-    fmt.Println("<-------- Confirmation Message -------->")
+	fmt.Println()
+	fmt.Println("<-------- Confirmation Message -------->")
 
 	fmt.Printf("Thanks %v %v for buying %v tickets. Your ticket is confirmed.\n", firstName, lastName, ticketToBuy)
 	fmt.Printf("Check your mailbox [%v] for further details.\n", userEmail)
 
+	availableTicket = availableTicket - ticketToBuy
+
+	fmt.Printf("Remaining Tickets: %v\n", availableTicket)
 }
