9abfafaaf86c1f58986a427c73538f69d651acca
Touhidul Shawan <touhidulshawan@gmail.com>
2022-02-12 16:13:06 +0600

    refactoring booking app

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -5,61 +5,81 @@ import (
 	"strings"
 )
 
+// greet the user
+func greetUser(conferenceName string, availableTicket uint32) {
+	fmt.Printf("Buy tickets for current conference: %v\n", conferenceName)
+	fmt.Printf("Available Tickets : %v\n", availableTicket)
+	fmt.Println("----------------------------------------")
+}
+
+// get all necessary information from user to book tickets
+func getUserInformation() (userFullName string, userEmail string, ticketToBuy uint32) {
+
+	fmt.Print("Enter your first name: ")
+	var firstName string
+	fmt.Scan(&firstName)
+
+	fmt.Print("Enter your last name: ")
+	var lastName string
+	fmt.Scan(&lastName)
+
+	fmt.Print("Enter your email: ")
+	fmt.Scan(&userEmail)
+
+	fmt.Print("How many tickets you want to buy: ")
+	fmt.Scan(&ticketToBuy)
+
+	userFullName = firstName + " " + lastName
+
+	return
+}
+
+func ticketConfirmationMessage(fullName string, userEmail string, ticketToBuy uint32) {
+
+	fmt.Println("\n<-------- Confirmation Message -------->")
+	fmt.Printf("Thanks %v for buying %v tickets.\n", fullName, ticketToBuy)
+	fmt.Printf("Check your mailbox [%v] for further details.\n", userEmail)
+}
+
+func allBookings(bookings []string) {
+
+	// print only first name of bookins user
+	// like [Touhidul Shawan] we only use Touhidul
+	// Filed splits the string with empty whitespace here
+	firstNames := []string{}
+	for _, booking := range bookings {
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+	fmt.Printf("Bookings : %v\n", firstNames)
+
+}
+
 func main() {
 	fmt.Println("Welcome to our Booking App")
 
 	conferenceName := "Love Yourself"
 	var availableTicket uint32 = 25
 	bookings := []string{}
+
 	for {
+		greetUser(conferenceName, availableTicket)
 
-		fmt.Printf("Buy tickets for current conference: %v\n", conferenceName)
-		fmt.Printf("Available Tickets : %v\n", availableTicket)
-		fmt.Println("----------------------------------------")
-
-		// take information from user
-
-		fmt.Print("Enter your first name: ")
-		var firstName string
-		fmt.Scan(&firstName)
-
-		fmt.Print("Enter your last name: ")
-		var lastName string
-		fmt.Scan(&lastName)
-
-		fmt.Print("Enter your email: ")
-		var userEmail string
-		fmt.Scan(&userEmail)
-
-		fmt.Print("How many tickets you want to buy: ")
-		var ticketToBuy uint32
-		fmt.Scan(&ticketToBuy)
+		var userFullName, userEmail, ticketToBuy = getUserInformation()
 
 		if ticketToBuy <= availableTicket {
 			availableTicket = availableTicket - ticketToBuy
-			bookings = append(bookings, firstName+" "+lastName)
+			bookings = append(bookings, userFullName)
 
 		} else {
 			fmt.Printf("Invalid tickets number. We have only %v tickets available\n", availableTicket)
 			continue
 		}
 
-		fmt.Println()
-		fmt.Println("<-------- Confirmation Message -------->")
+		ticketConfirmationMessage(userFullName, userEmail, ticketToBuy)
 
-		fmt.Printf("Thanks %v %v for buying %v tickets.\n", firstName, lastName, ticketToBuy)
-		fmt.Printf("Check your mailbox [%v] for further details.\n", userEmail)
-
-		// print only first name of bookins user
-		// like [Touhidul Shawan] we only use Touhidul
-		// Filed splits the string with empty whitespace here
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
-		fmt.Printf("Bookings : %v\n", firstNames)
-		fmt.Println("-------------------------------------------------")
+		// print all bookings
+		allBookings(bookings)
 
 		if availableTicket == 0 {
 			fmt.Printf("All Tickets Sold Out For %v Conference\n", conferenceName)
